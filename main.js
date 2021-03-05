a8c121d9f857ed2623ee79ea72f90bed2f44a673
attila <atti80@gmail.com>
2021-03-05 16:32:04 +0000

    Add data arrays, getRandomItem, getRandomMessage functions.

---
+++ main.js
@@ -0,0 +1,4 @@
+const msgData1 = ['Hi!', 'Hello!', 'Good day to You!', 'Welcome!', 'Hey!'];
+const msgData2 = ['Did you know?', 'Have you heard?', 'You won\'t believe this!', 'Picture this!'];
+const msgData3 = ['', '', '', '', ''];
+
